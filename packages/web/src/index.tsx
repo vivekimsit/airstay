544a8d5078e9e0524afb8dfc45d244d7da7b350f
Vivek Poddar <vivekimsit@gmail.com>
2018-06-20 21:34:06 +0100

    Init.

---
+++ packages/web/src/index.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import { ApolloProvider } from 'react-apollo';
+
+import registerServiceWorker from './registerServiceWorker';
+
+import { client } from './apollo';
+import { Routes } from "./routes";
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Routes />
+  </ApolloProvider>,
+  document.getElementById('root') as HTMLElement
+);
+registerServiceWorker();
