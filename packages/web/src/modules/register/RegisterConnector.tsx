f15002e647d3a781c8351bf566ae1e840aacffc5
Vivek Poddar <vivekimsit@gmail.com>
2018-07-08 19:03:10 +0100

    Adding controller

--- packages/web/src/modules/register/RegisterConnector.tsx
+++ packages/web/src/modules/register/RegisterConnector.tsx
@@ -1,16 +1,17 @@
 import * as React from "react";
+import { RegisterController } from "@airstay/controller";
+
 import { RegisterView } from "./ui/RegisterView";
 
 // container -> view
 // container -> connector -> view
 // controller -> connector -> view
 export class RegisterConnector extends React.PureComponent {
-  dummySubmit = async (values: any) => {
-    console.log(values);
-    return null;
-  };
-
   render() {
-    return <RegisterView submit={this.dummySubmit} />;
+    return (
+      <RegisterController>
+        {({ submit }) => <RegisterView submit={submit} />}
+      </RegisterController>
+    );
   }
 }
