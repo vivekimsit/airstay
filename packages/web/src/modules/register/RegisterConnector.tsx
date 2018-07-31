9e7de16fd7f431a8d3ab5aa8e44a11fa9242dec3
Vivek Poddar <vivekimsit@gmail.com>
2018-07-31 22:23:24 +0100

    Generic text component.

--- packages/web/src/modules/register/RegisterConnector.tsx
+++ packages/web/src/modules/register/RegisterConnector.tsx
@@ -1,17 +1,27 @@
 import * as React from "react";
 import { RegisterController } from "@airstay/controller";
+import { RouteComponentProps } from "react-router-dom";
 
 import { RegisterView } from "./ui/RegisterView";
 
 // container -> view
 // container -> connector -> view
 // controller -> connector -> view
+export class RegisterConnector extends React.PureComponent<
+  RouteComponentProps<{}>
+> {
+  onFinish = () => {
+    this.props.history.push("/m/confirm-email", {
+      message: "check your email to confirm your account"
+    });
+  };
 
-export class RegisterConnector extends React.PureComponent {
   render() {
     return (
       <RegisterController>
-        {({ submit }) => <RegisterView submit={submit} />}
+        {({ submit }) => (
+          <RegisterView onFinish={this.onFinish} submit={submit} />
+        )}
       </RegisterController>
     );
   }
