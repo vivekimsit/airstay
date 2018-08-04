ac62ba539be6072ecc9d52ca596bdb8923f231a6
Vivek Poddar <vivekimsit@gmail.com>
2018-08-04 09:54:16 +0100

    Listings api with auth middleware/

---
+++ packages/server/src/modules/listing/delete/resolvers.ts
@@ -0,0 +1,29 @@
+import { ResolverMap } from "../../../types/graphql-utils";
+import { Listing } from "../../../entity/Listing";
+
+export const resolvers: ResolverMap = {
+  Mutation: {
+    deleteListing: async (_, { id }, { session }) => {
+      // isAuthenticated(session);
+      const listing = await Listing.findOne({ where: { id } });
+
+      if (!listing) {
+        throw new Error("does not exist");
+      }
+
+      if (session.userId !== listing.userId) {
+        // log message
+        console.log(
+          `this user ${
+            session.userId
+          } is trying to delete a listing they don't own`
+        );
+        throw new Error("not authorized");
+      }
+
+      await Listing.remove(listing);
+
+      return true;
+    }
+  }
+};
